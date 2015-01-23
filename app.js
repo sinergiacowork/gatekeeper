789ef774ae99cd626f23e6819f7e46f4726c05cb
elCuervo <yo@brunoaguirre.com>
2015-01-23 14:38:58 -0200

    Full thing again

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+var express = require('express')
+var httpProxy = require('http-proxy');
+var Stream = require('node-rtsp-stream');
+
+var stream = new Stream({
+    name: 'Door',
+    streamUrl: process.env.RTSP_CONNECTION,
+    wsPort: 9999
+});
+
 var spawn = require("child_process").spawn;
+var proxy = httpProxy.createProxyServer({ ws: true });
 var app = require("express")();
 
 var ffmpeg = spawn("ffmpeg", [
@@ -10,8 +21,27 @@ var ffmpeg = spawn("ffmpeg", [
   "tmp/door.jpg"
 ]);
 
+const MPEG_CONNECTION = 'http://localhost:9999/';
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
 app.get("/door.jpg", function(req, res) {
   res.sendFile("door.jpg", { root: "./tmp" });
 });
 
-require("http").createServer(app).listen(process.env.PORT);
+app.get('/ws', function(req, res) {
+  proxy.web(req, res, { target: MPEG_CONNECTION });
+});
+
+var server = require('http').createServer(app);
+server.on('upgrade', function (req, socket, head) {
+  proxy.ws(req, socket, head, { target: MPEG_CONNECTION });
+})
+
+server.listen(process.env.PORT);
